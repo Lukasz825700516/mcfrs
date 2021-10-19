9f9856ca68568ff586f82fa7557878da3746b370
Lukasz <lukasz@lukaszm.xyz>
2021-10-19 09:03:18 +0200

    Rewroted scopes compiler
    Scopes compiler now uses primarly peeking_take_while to reduce
    unnesesary complexity of code.

--- src/mcfrs/compiler/scopes_compiler.rs
+++ src/mcfrs/compiler/scopes_compiler.rs
@@ -1,14 +1,15 @@
-use std::{collections::{HashMap, VecDeque}, ops::RangeFrom};
+use std::{iter::Peekable, ops::RangeFrom};
 
-use crate::mcfrs::{scope::Scope, util::get_indent};
+use itertools::Itertools;
+
+use crate::mcfrs::scope::Scope;
 
 pub struct ScopesCompiler<'a, I>
 where I: Iterator<Item = Scope<'a>>{
     source: I,
 
-    buffered_out: VecDeque<Scope<'a>>,
+    buffered: Vec<Scope<'a>>,
     next_anonymous_scope_name: RangeFrom<usize>,
-    compiled_scopes: HashMap<String, String>,
 }
 
 impl<'a, I> Iterator for ScopesCompiler<'a, I>
@@ -16,63 +17,24 @@ where I: Iterator<Item = Scope<'a>> {
     type Item = Scope<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.buffered_out.pop_front() {
-            Some(scope) => Some(scope),
+        match self.buffered.pop() {
             None => match self.source.next() {
-                Some(scope) => {
-                    let lines = scope.content.lines()
-                        .map(|l| l.trim_end())
-                        .filter(|l| l.len() > 0);
+                Some(mut scope) => {
+                    let lines = scope
+                        .content
+                        .clone();
+                    let lines = lines
+                        .lines()
+                        .peekable();
+                    let mut new_scopes: Vec<Scope<'a>> = Vec::new();
 
-                    let mut stack: Vec<Scope<'a>> = vec![Scope::new(scope.name.clone(), scope.namespace)];
-
-                    for line in lines {
-                        let indent = get_indent(line);
-
-                        if stack.len() < indent {
-                            let new_scope = Scope::new_unnamed(0, scope.namespace);
-                            stack.push(new_scope);
-                        } 
-                        while stack.len() > indent {
-                            let mut new_scope = stack.pop().unwrap();
-                            match self.compiled_scopes.get(&new_scope.content) {
-                                Some(reference_name) => {
-                                    stack.last_mut().unwrap().content += " ";
-                                    stack.last_mut().unwrap().content += reference_name.as_str();
-                                },
-                                None => {
-                                    new_scope.set_reference_id(self.next_anonymous_scope_name.next().unwrap());
-                                    stack.last_mut().unwrap().content += " ";
-                                    stack.last_mut().unwrap().content += new_scope.get_reference_name().as_str();
-                                    self.compiled_scopes.insert(new_scope.content.clone(), new_scope.get_reference_name());
-                                    self.buffered_out.push_back(new_scope);
-                                }
-                            }
-                        }
-
-                        stack.last_mut().unwrap().content += "\n";
-                        stack.last_mut().unwrap().content += line.trim_start();
-                    }
-
-                    while stack.len() > 0 {
-                        let mut new_scope = stack.pop().unwrap();
-                        match self.compiled_scopes.get(&new_scope.content) {
-                            Some(_) => {
-                            },
-                            None => {
-                                if stack.len() > 0 {
-                                    new_scope.set_reference_id(self.next_anonymous_scope_name.next().unwrap());
-                                }
-                                self.compiled_scopes.insert(new_scope.content.clone(), new_scope.get_reference_name());
-                                self.buffered_out.push_back(new_scope);
-                            }
-                        }
-                    }
-
-                    self.next()
+                    self.generate_scopes(&mut scope, lines, &mut new_scopes);
+                    self.buffered = new_scopes;
+                    Some(scope)
                 }
                 None => None
-            }
+            },
+            scope => scope
         }
     }
 }
@@ -92,10 +54,43 @@ where I: Iterator<Item = Scope<'a>> {
     pub fn new(source: I) -> Self {
         Self {
             source,
-            buffered_out: VecDeque::new(),
+            buffered: Vec::new(),
             next_anonymous_scope_name: (0 as usize)..,
-            compiled_scopes: HashMap::new(),
         }
+    }
+
+    pub fn generate_scopes<'b, J>(&mut self, scope: &mut Scope<'a>, mut lines: Peekable<J>, new_scopes: &mut Vec<Scope<'a>>) -> ()
+    where J: Iterator<Item = &'b str> {
+        let mut new_content = String::new();
+
+        while lines.peek().is_some() {
+            new_content += lines
+                .by_ref()
+                .peeking_take_while(|line| line.chars().next() != Some('\t'))
+                .join("\n")
+                .as_str();
+
+            let new_scope = lines
+                .by_ref()
+                .peeking_take_while(|line| line.chars().next() == Some('\t'))
+                .map(|line| &line[1..])
+                .collect::<Vec<_>>();
+
+            if new_scope.len() > 0 {
+                let mut scope = Scope::new_unnamed(self.next_anonymous_scope_name.next().unwrap(), scope.namespace);
+
+                new_content += " ";
+                new_content += scope
+                    .get_reference_name()
+                    .as_str();
+                new_content += "\n";
+
+                self.generate_scopes(&mut scope, new_scope.into_iter().peekable(), new_scopes);
+                new_scopes.push(scope);
+            }
+        }
+
+        scope.content = new_content;
     }
 }
 
